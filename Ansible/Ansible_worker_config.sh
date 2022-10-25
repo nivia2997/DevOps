f4beb462660484262fe1b39899c253bf199a24bf
nivia2997 <112555053+nivia2997@users.noreply.github.com>
2022-10-25 14:50:31 -0500

    Update Ansible_worker_config.sh

--- Ansible/Ansible_worker_config.sh
+++ Ansible/Ansible_worker_config.sh
@@ -8,7 +8,7 @@ sudo mv $user /etc/sudoers.d/
 sudo mkdir /home/$user/.ssh
 sudo chown -R $user:$user /home/$user/.ssh
 sudo chmod 700 /home/$user/.ssh
-sudo echo "hola" >> authorized_keys
+sudo echo "pega el id_rsa.pub aca" >> authorized_keys
 sudo chmod 600 authorized_keys
 sudo chown -R $user:$user authorized_keys
 sudo mv authorized_keys /home/$user/.ssh/
